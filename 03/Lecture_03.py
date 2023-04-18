a9e98141bfffe81c110b85d506199d4849a23243
Philipp Hennig <phennig@tuebingen.mpg.de>
2023-04-18 13:10:48 +0200

    playing with config

--- 03/Lecture_03.py
+++ 03/Lecture_03.py
@@ -3,6 +3,7 @@ import streamlit as st
 from matplotlib import pyplot as plt
 
 from tueplots import bundles
+
 plt.rcParams.update(bundles.beamer_moml())
 
 st.set_page_config(
@@ -11,4 +12,9 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={"About": "(c) Philipp Hennig, 2023"},
+    primaryColor="#A51E37",
+    backgroundColor="#FFFFFF",
+    secondaryBackgroundColor="#E1D7C1",
+    textColor="#32414B",
+    font="sans serif",
 )
--- 03/pages/01_Change_of_Measure.py
+++ 03/pages/01_Change_of_Measure.py
@@ -13,12 +13,18 @@ plt.rcParams.update(bundles.beamer_moml())
 plt.rcParams.update({"figure.figsize": (5, 3)})
 
 st.set_page_config(
-    page_title="Change of Measure",
+    page_title="Lecture 03",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={"About": "(c) Philipp Hennig, 2023"},
+    primaryColor="#A51E37",
+    backgroundColor="#FFFFFF",
+    secondaryBackgroundColor="#E1D7C1",
+    textColor="#32414B",
+    font="sans serif",
 )
+
 
 
 def sigmoid(loc: float, gain: float) -> Callable:
